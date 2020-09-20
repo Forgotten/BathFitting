7b7108cbb2162e2a34c8508c827f43c818ec2387
Leonardo <lzepeda@math.mit.edu>
2020-09-19 23:36:03 -0500

    feature: added (non-functional) setup file

--- setup.py
+++ setup.py
@@ -4,18 +4,21 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open('License.md') as f:
     license = f.read()
 
 setup(
-    name='bathfitting',
+    name='bath_fitting',
     version='0.1.0',
     description='Bath fitting via semi-definite relaxation package',
     long_description=readme,
     author='Leonardo Zepeda-Nunez',
     author_email='zepedanunez@wisc.edu',
     url='https://github.com/Forgotten/BathFitting',
-    #license=license,
+    license=license,
     install_requires=['numpy', 'scipy', 'cvxpy']
-    #packages=find_packages(exclude=('tests', 'docs'))
+    packages=setuptools.find_packages(),
+    classifiers=["Programming Language :: Python :: 3",
+                "License :: MIT License",
+                "Operating System :: OS Independent",],
 )
