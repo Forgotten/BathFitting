21918b007268ca50460ef09b2bea1d8f880c17af
Leonardo <lzepeda@math.mit.edu>
2020-12-17 20:47:50 -0600

    docs: added exra comments

--- bath_fitting/bath_fitting.py
+++ bath_fitting/bath_fitting.py
@@ -5,8 +5,18 @@ from bath_fitting.SDR_fitting import SDRFit
 
 def bath_fitting(K, z, Delta,
                  tol = 1.e-3,
-                 opt_tol = 1.e-6,  poles0 = [], 
+                 opt_tol = 1.e-9,  poles0 = [], 
                  verbose = False):
+    '''function to Fit the baths
+        inputs: K : the number of poles (generalized ones)
+                z : the sampling points in the imaginary axis
+                Delta : the bath that we are seeking to fit     
+                        which is sampled at the points in z
+                tol : tolerance for the ranks
+                opt_tol : optimization tolerance
+                poles0 : initial guess
+                verbose : flag to have further details
+        '''
 
     # if there is not initial guess randomly create one
     if type(poles0) == list:
@@ -17,12 +27,15 @@ def bath_fitting(K, z, Delta,
 
     # last two dimensions are the same
     assert Delta.shape[1] == Delta.shape[2]
-    # number of 
+    # number of samples matches with the sampling positions
     assert z.shape[0] == Delta.shape[0]
 
+    # we create an SDRFit object
     sdr_fitting = SDRFit(z, Delta, L, K)
 
-    print("defining the optimization function")
+    if verbose: 
+        print("defining the optimization function")
+    
     fun = lambda x: sdr_fitting(x, flag='grad')
 
     print("performing the optimization")
@@ -31,12 +44,16 @@ def bath_fitting(K, z, Delta,
                    jac = True,
                    options={'eps': opt_tol, 'disp': True})
 
-    print("optimization done")
+    if verbose: 
+        print("optimization done")
+    
     print("final value is %.6f"%(fun(res.x)[0]))
+    
     # compute the bath from the optimized poles
     Xbath = sdr_fitting(res.x, flag = 'bath')
 
     # extracting the V vectors with their corresponding energies
+    # and truncating them to the given tolerance
     V = []
     for ii in range(len(Xbath)):
         (Lambda, E) = np.linalg.eig(Xbath[ii])
--- tests/test_bath_fitting.py
+++ tests/test_bath_fitting.py
@@ -1,5 +1,7 @@
 # testing SDR class
-import context
+
+import context 	# not necessary if already installed
+				# using python install
 
 import cvxpy as cp
 import numpy as np
@@ -10,31 +12,43 @@ from datetime import datetime
 from bath_fitting.bath_fitting import bath_fitting
 from bath_fitting.SDR_fitting import SDRFit
 
-
+# number of poles
 K = 2
+# number of samples
 nz = 100
 
+# rank of each matrix
 rank = 3
 
+# equispace sampling 
 z = 1j*np.linspace(1, 10, nz)
 L = 9
-M = [ np.random.rand(L,L) for _ in range(K)]
-Eigs = [ np.linalg.eig(m@m.T) for m in M ]
-M_sym = [ X[:,:rank] @ (np.diag(e[:rank]) @ X[:,:rank].T)  for e, X in Eigs ]
 
+# we generate K different L x L matrices 
+M = [ np.random.rand(L,L) for _ in range(K)]
+
+# we cut-off the rank and we symmetrize the matrices
+Eigs = [ np.linalg.eig(m@m.T) for m in M ]
+M_sym = [ X[:,:rank] @ (np.diag(e[:rank]) @ X[:,:rank].T)  for e, X in Eigs]
+
+# we generate K number of poles (in this case they are fixed)
 poles = np.array([0.1, 1.9])
 
 diff =  1/(np.reshape(z, (-1, 1, 1, 1)) - np.reshape(poles, (1,K, 1, 1)))
 M_sym_np = np.array(M_sym).reshape((1, K, L, L))
 
+# we generate our data following
+# Delta(z_{\ell}) = sum_{k=1}^K \frac{M_{k}}{z_{\ell} - poles_k }
 Delta = np.sum(diff*M_sym_np, axis = 1)
 
+# define initial values for the poles 
 # poles_0 = 2*np.random.rand(K)
 poles_0 =  np.array([0.2, 1.8])
 
+# running the optimization process. 
 Result = bath_fitting(K, z, Delta,
          	          tol = 1.e-4,
-              		  opt_tol = 1.e-6,  poles0 = poles_0)
+              		  opt_tol = 1.e-9,  poles0 = poles_0)
 
 print("Relative error on the reconstruction is %.2g"%(\
 			np.linalg.norm(Result[0][0] @ Result[0][0].T - M_sym[0], 'fro')/
@@ -42,4 +56,5 @@ print("Relative error on the reconstruction is %.2g"%(\
 
 print("Relative error on the reconstruction is %.2g"%(\
 			np.linalg.norm(Result[0][1] @ Result[0][1].T - M_sym[1], 'fro')/
-			np.linalg.norm( M_sym[1], 'fro')))     
+			np.linalg.norm( M_sym[1], 'fro')))   
+
